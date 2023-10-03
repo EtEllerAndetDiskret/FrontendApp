1cfce08ae1d2ad167baa55d300fe040baf254987
Sadek <sade0027@stud.kea.dk>
2023-10-03 11:15:11 +0200

    Set up folder structure

---
+++ index.js
@@ -0,0 +1,3 @@
+//import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
+import "./navigo.js"  //Will create the global Navigo, with a few changes, object used below
+//import "./navigo.min.js"  //Will create the global Navigo object used below
