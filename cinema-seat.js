dbd4c523fd385804513e4ecb5166ae7c1a0db8bd
AlexanderFnug <alex314x@stud.kea.dk>
2023-10-04 11:10:06 +0200

    Seats without numbers and pop-up confirm buttom

--- cinema-seat.js
+++ cinema-seat.js
@@ -1,60 +1,101 @@
 // Get the seat container element
-const seatContainer = document.querySelector('.seat-container');
+const seatContainer = document.querySelector(".seat-container");
+
+// Create an object to store the seat numbers
+const seatNumbers = {};
 
 // Create a function to generate the seats
 function generateSeats() {
-    // Loop through each row
-    for (let i = 1; i <= 25; i++) {
-        // Create a row container
-        const row = document.createElement('div');
-        row.classList.add('row-container');
+  // Loop through each row
+  for (let i = 1; i <= 25; i++) {
+    // Create a row container
+    const row = document.createElement("div");
+    row.classList.add("row-container");
 
-        // Loop through each seat in the row
-        for (let j = 1; j <= 16; j++) {
-            // Create a seat element
-            const seat = document.createElement('div');
-            seat.classList.add('seat');
+    // Loop through each seat in the row
+    for (let j = 1; j <= 16; j++) {
+      // Create a seat element
+      const seat = document.createElement("div");
+      seat.classList.add("seat");
 
-            // Create a seat label element
-            const seatLabel = document.createElement('span');
-            seatLabel.classList.add('seat-label');
-            seatLabel.innerText = `${String.fromCharCode(64 + i)} ${j} `;
+      // Add the seat number to the seat object
+      seatNumbers[`${String.fromCharCode(64 + i)}${j}`] = false;
 
-            // Add the seat label to the seat element
-            seat.appendChild(seatLabel);
+      // Add a click event listener to the seat element
+      seat.addEventListener("click", () => {
+        // Toggle the selected class on the seat element
+        seat.classList.toggle("selected");
+        console.log(`clicked ${i}${String.fromCharCode(64 + j)}`);
+        // Toggle the seat number in the seat object
+        seatNumbers[`${String.fromCharCode(64 + i)}${j}`] =
+          !seatNumbers[`${String.fromCharCode(64 + i)}${j}`];
 
-            // Add the seat element to the row container
-            row.appendChild(seat);
+        // Get the booking container element
+        const bookingContainer = document.querySelector(".booking-container");
+
+        // Remove any existing selected seats label and confirmation button
+        bookingContainer.innerHTML = "";
+
+        // Get all the selected seat numbers
+        const selectedSeatNumbers = Object.keys(seatNumbers).filter(
+          (seatNumber) => seatNumbers[seatNumber]
+        );
+
+        // If there are selected seats, create a selected seats label and confirmation button
+        if (selectedSeatNumbers.length > 0) {
+          // Create a selected seats label element
+          const selectedSeatsLabel = document.createElement("div");
+          selectedSeatsLabel.classList.add("selected-seats-label");
+          selectedSeatsLabel.innerText = `Selected Seats: ${selectedSeatNumbers.join(
+            ", "
+          )}`;
+
+          // Add the selected seats label to the booking container
+          bookingContainer.appendChild(selectedSeatsLabel);
+
+          // Create a confirmation button element
+          const confirmBtn = document.createElement("button");
+          confirmBtn.classList.add("confirm-btn");
+          confirmBtn.innerText = "Confirm Seats";
+
+          // Add a click event listener to the confirmation button element
+          confirmBtn.addEventListener("click", () => {
+            // Log the selected seat numbers to the console
+            console.log(selectedSeatNumbers);
+
+            // Reset the seat numbers object
+            Object.keys(seatNumbers).forEach(
+              (seatNumber) => (seatNumbers[seatNumber] = false)
+            );
+
+            // Change the class of the selected seats to "booked"
+            seats.forEach((seat) => {
+              if (seat.classList.contains("selected")) {
+                seat.classList.remove("selected");
+                seat.classList.add("booked");
+              }
+            });
+
+            // Remove the selected seats label and confirmation button
+            bookingContainer.innerHTML = "";
+          });
+
+          // Add the confirmation button to the booking container
+          bookingContainer.appendChild(confirmBtn);
         }
+      });
 
-        // Add the row container to the seat container
-        seatContainer.appendChild(row);
+      // Add the seat element to the row container
+      row.appendChild(seat);
     }
-}
 
+    // Add the row container to the seat container
+    seatContainer.appendChild(row);
+  }
+}
+console.log(seatNumbers);
 // Call the generateSeats function to generate the seats
 generateSeats();
 
 // Get all the seat elements
-const seats = document.querySelectorAll('.seat');
-
-// Add a click event listener to each seat element
-seats.forEach(seat => {
-    seat.addEventListener('click', () => {
-        // Toggle the selected class on the seat element
-        seat.classList.toggle('selected');
-        console.log(seat, "selected");
-    });
-});
-
-// Get the book button element
-const bookBtn = document.querySelector('#book-btn');
-
-// Add a click event listener to the book button element
-bookBtn.addEventListener('click', () => {
-    // Get all the selected seat elements
-    const selectedSeats = document.querySelectorAll('.seat.selected');
-
-    // Log the selected seats to the console
-    console.log(selectedSeats);
-});
+const seats = document.querySelectorAll(".seat");
