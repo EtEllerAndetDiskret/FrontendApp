40f285249eece9f851b540bac54a37f5a5406fdb
Tohasim <toreh.simonsen@live.dk>
2023-10-04 13:10:19 +0200

    Movie overview page done for now

--- movie-overview/movie-overview.js
+++ movie-overview/movie-overview.js
@@ -1,3 +1,35 @@
-document.getElementById("init-button").addEventListener("click", initMovies);
+const movieURL = "http://localhost:8080/api/movies";
 
-async function initMovies() {}
+initMovies();
+async function initMovies() {
+	await loadMovies();
+}
+
+async function loadMovies() {
+	const movies = await fetch(movieURL).then((res) => res.json());
+	const moviePreviews = movies.map(
+		//TODO: [KINO-36] When navigo router is set up, the links should be done via router and eventbubling
+		(movie) => `
+			<div class="movie-preview">
+            <div id="img-container">
+                <img
+                    src=${movie.poster}
+                />
+                <div id="blur-overlay">
+                    <div id="banner-container">
+                    
+                        <a href=""><div class="banner" id="read-more">Læs mere</div></a>
+                        <div class="banner" id="find-ticket">Find billet</div>
+                    </div>
+                </div>
+            </div>
+            <p id="details">
+                <b>Title:</b> ${movie.title} (${movie.year}) <br />
+                <b>Genre: </b> ${movie.genre}
+            </p>
+        </div>
+        `
+	);
+	const moviePreviewsAsString = moviePreviews.join(" ");
+	document.getElementById("movies-container").innerHTML = moviePreviewsAsString;
+}
