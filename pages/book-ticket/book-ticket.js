f9fa853195d9a648b921350ec65be6e48544ab43
C856k <Carl856k@stud.kea.dk>
2023-10-07 09:07:15 +0200

    Began on the frontend to book-ticket

---
+++ pages/book-ticket/book-ticket.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",function() {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+    const dateInput = document.querySelector("#date");
+    const timeInput = document.querySelector("#time");
+
+    const selectedDate = dateInput.value;
+    const selectedTime = timeInput.value;
+
+    console.log("Valgt dag", selectedDate)
+    console.log("Valgt tidspunkt", selectedTime)
+    });
+});
