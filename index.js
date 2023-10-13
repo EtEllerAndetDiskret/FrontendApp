9559645063bc4633b175802ba92549b6e47d03cb
Sadek <sade0027@stud.kea.dk>
2023-10-13 15:19:31 +0200

    works without data, qr code and download ticket

--- index.js
+++ index.js
@@ -5,60 +5,90 @@ import { setActiveLink, loadHtml, renderHtml } from "./utils.js";
 
 import { initMovies } from "./pages/movie-overview/movie-overview.js";
 import { initMovieDetailed } from "./pages/movie-detailed/movie-detailed.js";
-
+import { initShowings } from "./pages/showings-of-movie/showings.js";
+import { initCinemaSeats } from "./pages/theater/cinema-seat.js";
+import { initLogin } from "./pages/login/login.js";
+import { initConfirmation } from "./pages/Booking-Confirmation/confirmation.js";
 window.addEventListener("load", async () => {
+  
 	const templateNotFound = await loadHtml("./pages/notFound/notFound.html");
 	const allMovies = await loadHtml(
 		"./pages/movie-overview/movie-overview.html"
 	);
+	const cinemaSeats = await loadHtml(
+		"./pages/theater/cinemaseat.html"
+	);
 	const movieDetailed = await loadHtml(
 		"./pages/movie-detailed/movie-detailed.html"
 	);
+	const movieShowings = await loadHtml(
+		"./pages/showings-of-movie/showings.html"
+	);
+	const bookingConfirmatin = await loadHtml(
+		"./pages/Booking-Confirmation/confirmation.html"
 
-	const router = new Navigo("/", { hash: true });
-	//Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
-	window.router = router;
+	)
 
-	router
-		.hooks({
-			before(done, match) {
-				setActiveLink("menu", match.url);
-				done();
-			},
-		})
-		.on({
-			"/": () =>
-				(document.getElementById("content").innerHTML = `
+
+  const router = new Navigo("/", { hash: true });
+  //Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
+  window.router = router;
+
+  router
+    .hooks({
+      before(done, match) {
+        setActiveLink("menu", match.url);
+        done();
+      },
+    })
+	  .on({
+		  "/": () => {
+		  (document.getElementById("content").innerHTML = `
         <h2>Welcome, to the future of cinema</h2>
         <p>Noget mere text der sikkert er super godt :)</p>
-        `),
-			"/movies/:id": (params) => {
-				const id = params.data.id;
-				renderHtml(movieDetailed, "content");
-				initMovieDetailed(id);
-			},
-			"/movies": () => {
-				renderHtml(allMovies, "content");
-				initMovies();
-			},
-		})
-		.notFound(() => {
-			renderHtml(templateNotFound, "content");
-		})
-		.resolve();
+        `); initLogin();},
+		  
+		  "/movies/:id": (params) => {
+			  const id = params.data.id;
+			  renderHtml(movieDetailed, "content");
+			  initMovieDetailed(id);
+		  },
+		  "/movies": () => {
+			  renderHtml(allMovies, "content");
+			  initMovies();
+		  },
+		  "/cinemaseats": () => {
+			  renderHtml(cinemaSeats, "content");
+			  initCinemaSeats();
+		  },
+		  "/confirmation": () => {
+			renderHtml(bookingConfirmatin, "content");
+			initConfirmation();
+		},
+
+
+		//   "/login": () => {
+		// 	  renderHtml(login, "content");
+		// 	  initLogin();
+		//   }
+	  })
+    .notFound(() => {
+      renderHtml(templateNotFound, "content");
+    })
+    .resolve();
 });
 
 window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
-	alert(
-		"Error: " +
-			errorMsg +
-			" Script: " +
-			url +
-			" Line: " +
-			lineNumber +
-			" Column: " +
-			column +
-			" StackTrace: " +
-			errorObj
-	);
+  alert(
+    "Error: " +
+      errorMsg +
+      " Script: " +
+      url +
+      " Line: " +
+      lineNumber +
+      " Column: " +
+      column +
+      " StackTrace: " +
+      errorObj
+  );
 };
